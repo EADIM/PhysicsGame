a298e5bf9c9c720377e0c43668cce3c8740fe5fe
Rubens Mendes <rubens_mendes_87@hotmail.com>
2020-11-12 17:00:30 -0300

    Added script to play button

---
+++ Assets/Scripts/Levels/Fase_02/Fase02_OnClickChangeGameState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fase02_OnClickChangeGameState : MonoBehaviour
+{
+
+    public Fase02_References references;
+
+    private Fase02_GameState gameState;
+
+    private void Start() {
+        gameState = references.GameState.GetComponent<Fase02_GameState>();
+    }
+
+    public void changeState(){
+
+        if(gameState.States[gameState.getExplorationName()]){
+            gameState.SwitchState(gameState.getSimulationName());
+        }
+        else if (gameState.States[gameState.getSimulationName()]){
+            gameState.SwitchState(gameState.getExplorationName());
+        }
+    }
+}
