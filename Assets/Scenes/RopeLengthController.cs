b12644988344aa19a89200904130bc557ea048cd
Lucas Laheras <28765277+LucasLaheras@users.noreply.github.com>
2020-12-02 16:29:33 -0300

    rope lenght variable

---
+++ Assets/Scenes/RopeLengthController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Obi;
+
+public class RopeLengthController : MonoBehaviour
+{
+
+	public float speed = 1;
+	ObiRopeCursor cursor;
+	ObiRope rope;
+
+	void Start()
+	{
+
+		cursor = (ObiRopeCursor)GetComponentInChildren(typeof(ObiRopeCursor));
+		rope = (ObiRope)cursor.GetComponent(typeof(ObiRope));
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.W))
+			cursor.ChangeLength(rope.restLength - speed * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.S))
+			cursor.ChangeLength(rope.restLength + speed * Time.deltaTime);
+	}
+}
+
