6db383cde08cf8bdb5886da6f432ef4b6e5ee2f3
Weverson <unifwpereira@fei.edu.br>
2020-12-13 21:27:27 -0300

    Images from phase 03 added

--- Assets/Scripts/Levels/Fase_03/UI/Fase03_SetProblemInfo.cs
+++ Assets/Scripts/Levels/Fase_03/UI/Fase03_SetProblemInfo.cs
@@ -50,26 +50,26 @@ public class Fase03_SetProblemInfo : MonoBehaviour
     }
 
     private void SetText(){
-        string[] sprites = {"<sprite=0>","<sprite=1>"};
+        string[] sprites = {"<sprite=0>","<sprite=1>", "<sprite=2>"};
         
         info = "" +
            "Se você chegou até aqui assumimos que você completou as outras fases e já está pronto para um desafio um pouco maior.\n" + 
            "O objetivo desta fase é escolher a quantidade de rotações necessárias para que o motor levante o bloco a uma altura que o faça deslizar até o buraco e caia na posição correta.\n" + 
            "O bloco será puxado por uma corda inextensível de massa desprezível e que faz parte de um conjunto de polias ideais, conforme a figura.\n" +  "\n\n\n\n" + 
-           sprites[0] + "\n\n\n\n" + 
+           sprites[0] + "\n\n\n\n\n" + 
            "Há três polias no total, uma fixa, sendo na verdade um motor que irá puxar a corda. Uma polia móvel, para oferecer ganho mecânico, e uma última polia fixa para direcionar a força. Despreze a força de atrito nas polias.\n" + 
            "Considere que o plano inclinado está a um ângulo de 45° com o piso.\n" + "\n\n\n\n" + 
-           sprites[1] + "\n\n\n\n" + 
+           sprites[1] + "\n\n\n\n\n" + 
            "Finalizada as rotações do motor, a corda irá se romper fazendo com que bloco deslize pelo plano inclinado e pelo piso. O coeficiente de atrito dinâmico entre o bloco e a plataforma que ele está vale 0,5.\n" + 
            "Caso o objeto ultrapasse o piso e caia, sua função será acertar o alvo que está a uma distância horizontal de 10m e uma distância vertical de 20m do piso. Despreze a resistência do ar.\n" + "\n\n\n\n" + 
-           sprites[1] + "\n\n\n\n";
+           sprites[2] + "\n\n\n\n";
 
         textUI.text = info;
     }
 
     private void SetText(string info){
         
-        string[] sprites = {"<sprite=0>","<sprite=1>"};
+        string[] sprites = {"<sprite=0>","<sprite=1>", "<sprite=2>"};
         
         textUI.text = info;
     }
